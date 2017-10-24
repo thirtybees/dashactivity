49fc88875324164ac45e988ab8ba8d9274bd072f
Michael Dekker <git@michaeldekker.nl>
2017-10-24 19:38:10 +0200

    Bump to 1.1.3

--- dashactivity.php
+++ dashactivity.php
@@ -46,7 +46,7 @@ class Dashactivity extends Module
     {
         $this->name = 'dashactivity';
         $this->tab = 'dashboard';
-        $this->version = '1.1.2';
+        $this->version = '1.1.3';
         $this->author = 'thirty bees';
         $this->push_filename = _PS_CACHE_DIR_.'push/activity';
         $this->allow_push = true;
